a50fa6f1245ed60bf30f03c96c3850faaf7e58bd
ANGELR27 <angelrodriguez270520@gmail.com>
2025-07-18 19:28:28 -0500

    ho

--- script.js
+++ script.js
@@ -1,3 +1,25 @@
-const boton = document.getElementById("mas");
+let contador = 0;
 
-boton.addEventListener;
+const menos = document.getElementById("menos");
+const mas = document.getElementById("mas");
+const valor = document.getElementById("valor");
+
+function actualizarContador() {
+  valor.textContent = contador;
+
+  if (contador >= 10) {
+    valor.style.color = "#ff4b5c";
+  } else {
+    valor.style.color = "#ffffffff";
+  }
+}
+
+menos.addEventListener("click", function () {
+  contador--;
+  valor.textContent = contador;
+});
+
+mas.addEventListener("click", function () {
+  contador++;
+  valor.textContent = contador;
+});
