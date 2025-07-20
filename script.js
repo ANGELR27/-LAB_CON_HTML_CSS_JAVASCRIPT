28bc219c6e2e0b1ac8b58ab59d69500df7fb4f6b
ANGELR27 <angelrodriguez270520@gmail.com>
2025-07-20 08:44:26 -0500

    fnl

--- script.js
+++ script.js
@@ -1,25 +1,31 @@
-let contador = 0;
+document.addEventListener("DOMContentLoaded", function () {
+  let contador = 0;
 
-const menos = document.getElementById("menos");
-const mas = document.getElementById("mas");
-const valor = document.getElementById("valor");
+  const menos = document.getElementById("menos");
+  const mas = document.getElementById("mas");
+  const valor = document.getElementById("valor");
 
-function actualizarContador() {
-  valor.textContent = contador;
+  function actualizarContador() {
+    valor.textContent = contador;
 
-  if (contador >= 10) {
-    valor.style.color = "#ff4b5c";
-  } else {
-    valor.style.color = "#ffffffff";
+    if (contador % 10 === 0 && contador !== 0) {
+      valor.style.color = "#ff4b5c";
+    } else {
+      valor.style.color = "#000000";
+    }
   }
-}
 
-menos.addEventListener("click", function () {
-  contador--;
-  valor.textContent = contador;
-});
+  menos.addEventListener("click", function () {
+    if (contador > 0) {
+      contador--;
+      actualizarContador();
+    }
+  });
 
-mas.addEventListener("click", function () {
-  contador++;
-  valor.textContent = contador;
+  mas.addEventListener("click", function () {
+    contador++;
+    actualizarContador();
+  });
+
+  actualizarContador();
 });
